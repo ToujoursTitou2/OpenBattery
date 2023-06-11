6687baeff75705d5f7b537d5c4ddd1533b5ad98b
Titou <129084186+ToujoursTitou2@users.noreply.github.com>
2023-06-11 16:56:54 +0200

    Add files via upload

---
+++ inputscan.h
@@ -0,0 +1,10 @@
+#ifndef INPUTSCAN_H
+#define INPUTSCAN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+void clearBuffer();
+char charScan(char *input, int size);
+int intScan();
+double doubleScan();
+#endif
---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SIZE 15
+
+void capacity();
+void status();
+void energy_full();
+void energy_now();
+void technology();
+void voltage_now();
+void type();
+void online();
+void serial_number();
+unsigned short countElementsInFolder(const char *folderPath, const char *excludedName);
+void battery_quantity();
+void cycle_count();
+void handleSignal(int signal);
+
+#endif
