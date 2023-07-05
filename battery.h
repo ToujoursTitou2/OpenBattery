365cec3c6580f5e9c7d3c2c9201a646d6ce557a9
Titou <129084186+ToujoursTitou2@users.noreply.github.com>
2023-07-05 16:38:42 +0200

    Add files via upload

---
+++ battery.h
@@ -0,0 +1,27 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define SIZE 15
+#define PATH_SIZE 50
+#define VERSION 2.9
+
+void help();
+void capacity(char *folderName);
+void status(char *folderName);
+void energy_full(char *folderName);
+void energy_now(char *folderName);
+void technology(char *folderName);
+void voltage_now(char *folderName);
+void type();
+void online();
+void serial_number(char *folderName);
+unsigned short countElementsInFolder(const char *folderPath, const char *excludedName);
+void battery_quantity();
+void cycle_count(char *folderName);
+void manufacturer(char *folderName);
+void model_name(char *folderName);
+void energy_full_design(char *folderName);
+void voltage_min_design(char *folderName);
+void version();
+
+#endif
